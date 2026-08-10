9bbde69082687b0dfb3922952fb20c78680cdba7
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:33:43 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/http/spring/boot/client/HttpClientConnectionProperties.java
+++ src/main/java/org/apache/http/spring/boot/client/HttpClientConnectionProperties.java
@@ -24,7 +24,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 /**
  * TODO
  * 
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @ConfigurationProperties(prefix = HttpClientConnectionProperties.PREFIX)
 public class HttpClientConnectionProperties {
